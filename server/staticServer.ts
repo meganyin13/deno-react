c2448b17379f18405a713e4deebfce72ec6c74e5
Megan Yin <my474@cornell.edu>
2020-05-20 00:39:04 -0400

    add top-level scripts

--- server/staticServer.ts
+++ server/staticServer.ts
@@ -1,8 +1,10 @@
+// @ts-ignore deno import
 import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
 
 const port = 3000;
 const app = new expressive.App();
 app.use(expressive.static_("./public"));
 app.use(expressive.bodyParser.json());
+// @ts-ignore deno supports top-level await!
 const server = await app.listen(port);
 console.log("app listening on port " + server.port);
