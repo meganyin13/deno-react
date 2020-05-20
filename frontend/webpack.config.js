d1dfa90bb5bdd22c49c01a4b52ebd1cd0e77fe13
Megan Yin <my474@cornell.edu>
2020-05-19 23:15:21 -0400

    add CRA template

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -29,6 +29,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
+        }
+      }
     ],
   },
   plugins: [
