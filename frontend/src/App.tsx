34ed62cde8f1e4a6f8bfac3357a96baa18d51ff6
Megan Yin <my474@cornell.edu>
2020-05-23 23:18:22 -0400

    add missing dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,9 +8,9 @@ export default (): React.ReactElement => (
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
+        <code>src/App.tsx</code>
+        and save to reload.
+      </p>
       <a
         className="App-link"
         href="https://reactjs.org"
