d1e0b565e14c0bb2d741b78862e8d974fae85e9f
Megan Yin <my474@cornell.edu>
2020-05-24 21:04:35 -0400

    Update staticServer.ts

--- server/staticServer.ts
+++ server/staticServer.ts
@@ -1,10 +1,17 @@
 // @ts-ignore deno import
-import * as expressive from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
+import { Application, send, Context } from "https://deno.land/x/oak/mod.ts";
 
 const port = 3000;
-const app = new expressive.App();
-app.use(expressive.static_("./public"));
-app.use(expressive.bodyParser.json());
+const app = new Application();
+
+app.use(async (ctx: Context) => {
+  await send(ctx, ctx.request.url.pathname, {
+    root: `./public`,
+    index: "index.html",
+  });
+});
+
+console.log("app listening on port " + port);
 // @ts-ignore deno supports top-level await!
-const server = await app.listen(port);
-console.log("app listening on port " + server.port);
+await app.listen({ port });
+
