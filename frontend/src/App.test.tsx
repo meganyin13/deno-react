a96dc42e8e5687e6bba2ef5ba6a342211e23a66e
Megan Yin <my474@cornell.edu>
2020-06-10 21:43:34 -0400

    add test script

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { toBeInTheDocument } from '@testing-library/jest-dom/matchers'
+
+expect.extend({ toBeInTheDocument })
 
 test('renders learn react link', () => {
   render(<App />);
