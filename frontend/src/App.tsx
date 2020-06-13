f3d440f6e85cf1947d5ea70605f8caab70baea55
Megan Yin <my474@cornell.edu>
2020-06-12 22:43:39 -0400

    Migrate to deno lint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 export default (): React.ReactElement => (
   <div className="App">
@@ -8,9 +8,9 @@ export default (): React.ReactElement => (
       <img src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
-        {' '}
+        {" "}
         <code>src/App.tsx</code>
-        {' '}
+        {" "}
         and save to reload.
       </p>
       <a
